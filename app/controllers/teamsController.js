be0536309c073b0d6f91ac335249c3fb859d5025
Victor <victoraweb@gmail.com>
2016-04-08 01:46:08 -0300

    Team [PUT ]

--- app/controllers/teamsController.js
+++ app/controllers/teamsController.js
@@ -2,19 +2,6 @@ var models = require('../models/index');
 
 var teamsController = function() {
 
-  var mid = function(req) {
-    models.Team.find({where: {id: req.params.id}}).then(team => {
-      if (team) {
-        return team;
-      } else {
-        return null;
-      }
-    }).catch(err => {
-      return err;
-    });
-
-  };
-
   var get = function(req, res, next) {
     var query = {};
     if (req.query.name) {
@@ -59,20 +46,28 @@ var teamsController = function() {
     return next();
   };
 
-  // var put = function(req, res, next) {
-  //   if (!req.body.hasOwnProperty('name')) {
-  //     res.send(500);
-  //     return next();
-  //   }
-  //   var team = findProductById(req);
-  //   if (team) {
-  //     team.name = req.body.name;
-  //     res.send(200, team);
-  //   } else {
-  //     res.send(404, 'Product not found');
-  //   }
-  //   return next();
-  // };
+  var put = function(req, res, next) {
+    var team = models.Team.find({
+      where: {id: req.params.id}
+    }).then(team => {
+      if (team) {
+        team.update({
+            name: req.body.name,
+            coach: req.body.coach
+          }).then(team => {
+            res.send(200, team);
+          }).catch(err => {
+            res.send(500, err);
+          });
+      } else {
+        res.send(404, 'Team not found');
+      }
+    }).catch(err => {
+      res.send(500, err);
+    });
+
+    return next();
+  };
 
   // var del = function(req, res, next) {
   //   that.store = that.store.filter(function(p) {
@@ -83,11 +78,10 @@ var teamsController = function() {
   // };
 
   return {
-    mid: mid,
     get: get,
     post: post,
-    getById: getById
-    // put: put,
+    getById: getById,
+    put: put,
     // del: del
   };
 };
--- app/routes/teamRoutes.js
+++ app/routes/teamRoutes.js
@@ -5,7 +5,7 @@ var teamRouter = function(server) {
   server.get('api/v1/teams', team.get);
   server.get('api/v1/teams/:id', team.getById);
   server.post('api/v1/teams', team.post);
-  // server.put('api/v1/teams/:id', team.put);
+  server.put('api/v1/teams/:id', team.put);
   // server.del('api/v1/teams/:id', team.del);
 
   return server;
