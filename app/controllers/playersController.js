10fdfdff5b9ecf0dff586aa993674eb8a3e2bc8b
Victor <victoraweb@gmail.com>
2016-04-11 16:29:31 -0300

    Player [GET:id]

--- app/controllers/playersController.js
+++ app/controllers/playersController.js
@@ -2,6 +2,17 @@ var models = require('../models/index');
 
 var playersController = function() {
 
+  var mid = function(req, res) {
+    return models.Player.find({
+      where: {
+        id: req.params.id,
+        TeamId: req.params.teamId
+      }
+    }).then(result => {
+      return result;
+    });
+  };
+
   var get = function(req, res, next) {
     var query = {};
     if (req.query.name) {
@@ -25,8 +36,22 @@ var playersController = function() {
     return next();
   };
 
+  var getById = function(req, res, next) {
+    mid(req, res).then(result => {
+      if (result) {
+        res.send(200, result);
+      } else {
+        res.send(404, 'Player not found');
+      }
+    }).catch(err => {
+      res.send(500, err);
+    });
+    return next();
+  };
+
   return {
-    get: get
+    get: get,
+    getById: getById
   };
 };
 
--- app/routes/playerRoutes.js
+++ app/routes/playerRoutes.js
@@ -4,6 +4,7 @@ var playerRouter = function(server) {
   var V1_0_0 = '1.0.0';
 
   server.get({path: 'api/teams/:teamId/players', version: V1_0_0}, player.get);
+  server.get({path: 'api/teams/:teamId/players/:id', version: V1_0_0}, player.getById);
 
   return server;
 
